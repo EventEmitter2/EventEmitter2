b3bfcbdf3af5d2f017cd8db902ca196bd8b426bc
hij1nx <fernmicro@gmail.com>
2011-05-31 00:04:05 -0400

    readme

---
+++ EventEmitter2.js
@@ -0,0 +1,312 @@
+var eyes = require('eyes'),
+    assert = require('assert');
+
+EventEmitter = function(conf) {
+  if(conf) {
+    
+    if(conf.delimiter === '*') {
+      throw new Error('The event can not be delimited by the "*" (wild-card) character.')
+    }
+
+  }
+  this._delimiter = conf ? conf.delimiter : '/';
+  this._events = {};
+};
+
+EventEmitter.prototype.isArray = function(v) {
+  return ~({}).toString.call(v).indexOf('Array');
+}
+
+EventEmitter.prototype.addListener = function(event, listener, ttl) {
+
+  var name, ns = this._events;
+
+  // Signal that a new listener is being added.
+  this.emit('newListener', event, listener);
+
+  // the name has a delimiter
+  if(~event.indexOf(this._delimiter)) {
+
+    //split the name into an array
+    name = event.split(this._delimiter);
+    
+    // continue to build out additional namespaces and attach the listener to them
+    while(name.length) {
+      
+      // get the namespace
+      var n = name.shift();
+      ns = ns[n] || (ns[n] = {});
+      
+      // if this is a wild card or the completed ns, add the event
+      if(name.length === 0) { 
+        ns._listeners ? ns._listeners.push(listener) : ns._listeners = [listener];
+        ns._ttl = ttl;
+        ns._ttd = 0;
+      }
+    }
+  }
+  
+  // if the name does not have a delimiter
+  else {
+
+    // get a handle to the event
+    var e = ns[event] || (ns[event] = {});
+
+    e._listeners ? e._listeners.push(listener) : e._listeners = [listener];
+    e._ttl = ttl;
+    e._ttd = 0;
+  }
+
+};
+
+EventEmitter.prototype.on = EventEmitter.prototype.addListener;
+
+EventEmitter.prototype.once = function() {
+  this.addListener(arguments, 1);
+};
+
+EventEmitter.prototype.emit = function(event) {
+
+  // get all the args except the event, make it a real array
+  var args = [].slice.call(arguments).slice(1);
+
+  // if there is a delimiter in the event name
+  if(~event.indexOf(this._delimiter)) {
+
+    //split the name into an array
+    name = event.split(this._delimiter);
+
+    var explore = [this._events],
+        invoked = false;
+        
+    for (var i = 0; i < name.length; i++) {
+      //
+      // Iterate over the parts of the potentially namespaced
+      // event. 
+      //
+      //     emit('foo/*/bazz') ==> ['foo', '*', 'bazz'] 
+      //
+      var part = name[i], newSets = [], removeAt = [];
+      
+      for (var j = 0; j < explore.length; j++) {
+        //
+        // Iterative "unkown" set exploration: Iterate over each "unknown"
+        // set of objects in the events tree. If a wildcard is discovered, 
+        // append that object to the unknown set and continue exploration.
+        //
+        var ns = explore[j];
+        
+        if (i === name.length - 1) {
+          //
+          // Then if we are at the end of the iteration
+          // invoke all of the listeners, if not, continue
+          // iterating deeper in the object
+          //
+          if (part === '*') {
+            for (var key in ns) {
+              //
+              // Remark: This could cause some collisions for `_listeners`,
+              // `_ttl`, and `_ttd`. 
+              //
+              if (ns[key] && ns[key]._listeners) {
+                for (var k = 0; k < ns[key]._listeners.length; k++) {
+                  ns[key]._listeners[k].apply(this, args);
+                }
+              }
+            }
+            invoked = true;
+          }
+          else {
+            if (ns[part] && ns[part]._listneners) {
+              for (var k = 0, l = ns[part]._listeners.length; k < l; k++) {
+                ns[part]._listeners[k].apply(this, args);
+              }
+              invoked = true;
+            }
+            
+            if (ns['*'] && ns['*']._listeners) {
+              for (var k = 0, l = ns['*']._listeners.length; k < l; k++) {
+                ns['*']._listeners[k].apply(this, args);
+              }
+              invoked = true;
+            }
+          }
+        }
+        else {
+          if (part !== '*') {
+            if (!ns[part] && !ns['*']) {
+              //
+              // If it's not a wild card and there isn't a wild
+              // card stored and the exact key isn't at the 
+              // next step of the events object, break out 
+              // of the loop and end evaluation.
+              //
+              continue;
+            }
+
+            if (ns[part]) {
+              //
+              // If it's not a wild card, but there is an exact
+              // match for this part of the namespaced event.
+              //
+              if (ns['*']) {
+                newSets.push(ns['*']);
+              }
+
+              explore[j] = explore[j][part];
+            }
+            else if (ns['*']) {
+              //
+              // If the part of the namespaced event is not a wildcard,
+              // but the set we are currently exploring has a wildcard
+              // at this level, nest deeper for that particular set.
+              //
+              explore[j] = explore[j]['*'];
+              
+              if (ns['*']._listeners) {
+                for (var k = 0, l = ns['*']._listeners.length; k < l; k++) {
+                  ns['*']._listeners[k].apply(this, args);
+                }
+                invoked = true;
+              }
+            }
+          } 
+          else {
+            //
+            // Otherwise, this part of the namespaced event is a 'wildcard',
+            // in which case, we iterate over the keys of the current set,
+            // and add those objects to the set to be added to the "unknown" set
+            // after this level of exploration has completed.
+            //
+            for (var key in ns) {
+              if (ns.hasOwnProperty(key)) {
+                newSets.push(ns[key]);
+              }
+            }
+
+            if (ns['*'] && ns['*']._listeners) {
+              for (var k = 0, l = ns['*']._listeners.length; k < l; k++) {
+                ns['*']._listeners[k].apply(this, args);
+              }
+              invoked = true;
+            }
+            
+            removeAt.push(j);
+          }
+        }
+      }
+      
+      for (var j = 0; j < removeAt.length; j++) {
+        //
+        // Remove stale sets that are no longer of interest.
+        //
+        explore.splice(j, 1);
+      }
+      
+      if (newSets.length) {
+        //
+        // If this level of exploration has yielded any new sets
+        // to be explored, then concatenate those sets to the "unknown" sets.
+        //
+        explore = explore.concat(newSets);
+      } 
+    }
+    
+    return invoked;
+  }
+
+  // if the name does not have a delimiter
+  else {
+    
+    if (!this._events[event]) {
+      return false;
+    }
+    
+    // get a handle to the listeners
+    var listeners = this._events[event]._listeners || null;
+    
+    if (!listeners) {
+      return false;
+    }
+    
+    // fire off each of them
+    for(var i = 0, l = listeners.length; i < l; i++) {
+      listeners[i].apply(this, args);
+    }
+    
+  }
+  
+}
+
+EventEmitter.prototype.removeListener = function() {};
+EventEmitter.prototype.removeAllListeners = function() {};
+
+EventEmitter.prototype.listeners = function() {};
+
+// test
+
+
+
+console.log('start');
+
+
+var tests = {
+ 'foo:*': 4,
+ 'foo:*:bar': 2,
+ 'foo:*:*': 3,
+ 'foo:bar:bar': 2
+}
+
+Object.keys(tests).forEach(function (event) {
+  var vat = new EventEmitter(),
+      count = 0;
+
+  vat.on(event, function () {
+    eyes.inspect(arguments, event);
+    count++;
+  });
+
+  eyes.inspect(event, 'Beginning test for');
+
+  vat.emit('foo/box/bar', 1, 2, 3, 4);
+  vat.emit('foo/bar/*', 5, 6, 7, 8);
+  vat.emit('foo/*/*', 9, 10, 11, 12);
+  vat.emit('foo/*');
+  
+  assert.equal(count, tests[event]);
+});
+
+  // -> foo/*/bar/*/aff
+  //     |  |   | |   |
+  //     |  |   | |   |
+  //     |  |   | |   |
+  //     |  |   | |   |
+  //     |  |   | |   |
+  // -> foo/x/bar/x/aff
+  //        |     |   |
+  //        e     e   e
+
+  // FAIL
+  
+  //    foo/*/bar/*/aff
+  //     |  |   | |   |
+  //     |  |   | e   e
+  //     |  |   |
+  //     m  |   m
+  //     |  |   |
+  // -> foo/x
+  //        |
+  //        e
+  
+  // FAIL
+  
+  //    foo/*
+  //     |  | 
+  //     |  | 
+  //     |  | 
+  //     m  | 
+  //     |  | 
+  // -> foo
+  //        |
+  //        e
+
---
+++ README.md
@@ -0,0 +1,81 @@
+
+## EventEmitter2
+
+When an `EventEmitter` instance experiences an error, the typical action is
+to emit an `error` event.  Error events are treated as a special case in node.
+If there is no listener for it, then the default action is to print a stack
+trace and exit the program.
+
+All EventEmitters emit the event `newListener` when new listeners are
+added.
+
+#### emitter.addListener(event [, key], listener)
+#### emitter.on(event, listener)
+
+Adds a listener to the end of the listeners array for the specified event.
+
+```javascript
+    server.on('get', function(value) {
+      console.log('a value was got!');
+    });
+```
+
+#### emitter.once(event, listener)
+
+Adds a **one time** listener for the event. The listener is invoked only the first time the event is fired, after which it is removed.
+
+```javascript
+    server.once('get', function (value) {
+      console.log('Ah, we have our first value!');
+    });
+```
+
+#### emitter.removeListener(event, listener)
+
+Remove a listener from the listener array for the specified event. **Caution**: changes array indices in the listener array behind the listener.
+
+```javascript
+    var callback = function(value) {
+      console.log('someone connected!');
+    };
+    server.on('get', callback);
+    // ...
+    server.removeListener('get', callback);
+```
+
+#### emitter.removeAllListeners([event])
+
+Removes all listeners, or those of the specified event.
+
+
+#### emitter.setMaxListeners(n)
+
+By default EventEmitters will print a warning if more than 10 listeners are added to it. This is a useful default which helps finding memory leaks. Obviously not all Emitters should be limited to 10. This function allows that to be increased. Set to zero for unlimited.
+
+
+#### emitter.listeners(event)
+
+Returns an array of listeners for the specified event. This array can be manipulated, e.g. to remove listeners.
+
+```javascript
+    server.on('get', function (value) {
+      console.log('someone connected!');
+    });
+    console.log(console.log(server.listeners('get')); // [ [Function] ]
+```
+
+#### emitter.emit(event, [arg1], [arg2], [...])
+
+Execute each of the listeners in order with the list of arguments.
+
+## Licence
+
+(The MIT License)
+
+Copyright (c) 2011 hij1nx <http://www.twitter.com/hij1nx>, indexzero <http://www.twitter.com/indexzero>, nodejitsu <http://www.twitter.com/nodejitsu>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
