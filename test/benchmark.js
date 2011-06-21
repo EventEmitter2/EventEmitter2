1aa3fe8a8f213addb9122c353a3e8d72b1a50ba8
Jameson Lee <jamesonjlee@gmail.com>
2011-06-21 19:13:16 -0400

    [test] fixed benchmark, basicEvents

--- test/benchmark.js
+++ test/benchmark.js
@@ -164,5 +164,36 @@ module.exports = testCase({
     test.expect(300);
     test.done();
   },
+
+  '5. add 10000 listener, emit 300 times with EE' : function (test) {
+    var iterations = 100;
+    var len = 10;
+    var EE = require('events').EventEmitter;
+    var emitter = new EE();
+
+    console.time('t5');
+    for (var i = 0; i < len; i++) {
+      for (var j = 0; j < len; j++){
+        for (var k = 0; k < len; k++){
+          for (var l = 0; l < len; l++){
+            emitter.on([i,j,k,l].join('.'), function () { test.ok(true,'emit') });
+          }
+        }
+      }
+    }
+
+    while (iterations--) {
+      emitter.emit('1.8.6.3');
+      emitter.emit('5.2.9.1');
+      emitter.emit('9.5.3.8');
+    //  emitter.emit('2.*.5');
+    //  emitter.emit('7.3.*');
+    }
+
+    console.timeEnd('t5');
+    test.expect(300);
+    test.done();
+  },
 });
+
 
