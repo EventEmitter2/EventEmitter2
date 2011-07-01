b93dff120d26c26d86cb04cca934c99a13c5c221
Jameson Lee <jamesonjlee@gmail.com>
2011-06-30 20:39:32 -0400

    [test] ee2 passes all simpleEvents, new test folder layout

--- test/benchmarks/benchmark-EE2A.js
+++ test/benchmarks/benchmark-EE2A.js
@@ -4,14 +4,14 @@ module.exports = testCase({
 
   'Add 50000 listener, emit 15k times' : function (test) {
 
-    var EventEmitter = require('../../test/ee_tmpvar').EventEmitter;
+    var EventEmitter = require('../../test/ee_idx0').EventEmitter2;
 
-    var EventsManager = new EventEmitter;
+    var EventsManager = new EventEmitter();
 
     var iterations = 10000;
     var len = 10;
 
-    console.time('EEtmpvar');
+    console.time('EEidx');
     for (var i = 0; i < len; i++) {
       for (var j = 0; j < len; j++){
         for (var k = 0; k < len; k++){
@@ -32,7 +32,7 @@ module.exports = testCase({
       EventsManager.emit('9.5.3.8');
     }
 
-    console.timeEnd('EEtmpvar');
+    console.timeEnd('EEidx');
     test.expect(150000);
     test.done();
   }
--- test/newtest.bash
+++ test/newtest.bash
@@ -4,7 +4,7 @@ then
   rm -f newtest.tmp
 fi
 
-for i in 'benchmark-EE2C.js' 'benchmark-EE2D.js' 'benchmark-EE2B.js'
+for i in 'benchmark-EE2A.js' 'benchmark-EE2C.js' 'benchmark-EE2D.js' 'benchmark-EE2B.js'
   do 
     nodeunit benchmarks/$i | sed '/32mOK/d' | sed '/^$/d' | sed 's/\[1m//g' | sed 's/\[22m//g' >> newtest.tmp
   done
