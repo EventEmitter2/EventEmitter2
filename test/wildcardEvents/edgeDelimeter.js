a61cddfe7977bf4419e80556c5cd138cff67a3e6
Andrei Shevel <andrei.shevel@tispr.com>
2016-09-06 00:06:15 +0300

    unit tests

---
+++ test/wildcardEvents/edgeDelimeter.js
@@ -0,0 +1,98 @@
+var simpleEvents = require('nodeunit').testCase;
+
+if(typeof require !== 'undefined') {
+  EventEmitter2 = require('../../lib/eventemitter2').EventEmitter2;
+} else {
+  EventEmitter2 = window.EventEmitter2;
+}
+
+module.exports = simpleEvents({
+
+  '1. delimeter at the start': function (test) {
+    var emitter = new EventEmitter2({
+      wildcard: true
+    });
+
+    emitter.on('.ns1.ns2', function () {
+      test.ok(true, 'event was raised');
+    });
+
+    emitter.emit('.ns1.ns2');
+
+    test.expect(1);
+    test.done();
+  },
+  '2. delimeter at the middle': function (test) {
+    var emitter = new EventEmitter2({
+      wildcard: true
+    });
+
+    emitter.on('ns1..ns3', function () {
+      test.ok(true, 'event was raised');
+    });
+
+    emitter.emit('ns1..ns3');
+
+    test.expect(1);
+    test.done();
+  },
+  '3. delimeter at the end': function (test) {
+    var emitter = new EventEmitter2({
+      wildcard: true
+    });
+
+    emitter.on('ns1.ns2.', function () {
+      test.ok(true, 'event was raised');
+    });
+
+    emitter.emit('ns1.ns2.');
+
+    test.expect(1);
+    test.done();
+  },
+  '4. custome delimeter at the start': function (test) {
+    var emitter = new EventEmitter2({
+      wildcard: true,
+      delimeter: '/'
+    });
+
+    emitter.on('/ns1/ns2', function () {
+      test.ok(true, 'event was raised');
+    });
+
+    emitter.emit('/ns1/ns2');
+
+    test.expect(1);
+    test.done();
+  },
+  '5. custome delimeter at the middle': function (test) {
+    var emitter = new EventEmitter2({
+      wildcard: true,
+      delimeter: '/'
+    });
+
+    emitter.on('ns1//ns3', function () {
+      test.ok(true, 'event was raised');
+    });
+
+    emitter.emit('ns1//ns3');
+
+    test.expect(1);
+    test.done();
+  },
+  '6. custome delimeter at the end': function (test) {
+    var emitter = new EventEmitter2({
+      wildcard: true
+    });
+
+    emitter.on('ns1/ns2/', function () {
+      test.ok(true, 'event was raised');
+    });
+
+    emitter.emit('ns1/ns2/');
+
+    test.expect(1);
+    test.done();
+  }
+
+});
