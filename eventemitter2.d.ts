dd117523fe20c50b93607a1759f66eaf3d585ddc
Dmitriy Mozgovoy <robotshara@gmail.com>
2020-04-19 14:47:26 +0300

    Fixed typings; Added symbol events support for listenTo&stopListening methods;

--- eventemitter2.d.ts
+++ eventemitter2.d.ts
@@ -137,7 +137,7 @@ export declare class EventEmitter2 {
     listenTo(target: GeneralEventEmitter, events: event | eventNS, options?: ListenToOptions): this;
     listenTo(target: GeneralEventEmitter, events: (event | eventNS)[], options?: ListenToOptions): this;
     listenTo(target: GeneralEventEmitter, events: Object, options?: ListenToOptions): this;
-    stopListening(target?: GeneralEventEmitter, event | eventNS?: String): Boolean;
+    stopListening(target?: GeneralEventEmitter, event?: event | eventNS): Boolean;
     hasListeners(event?: String): Boolean
     static once(emitter: EventEmitter2, event: event | eventNS, options?: OnceOptions): CancelablePromise<any[]>;
     static defaultMaxListeners: number;
