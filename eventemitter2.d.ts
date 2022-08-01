6ae4d9881ad8bafb3bb3dfe2e8175d973c0bb5af
raviteja83 <ravi.theja@100ms.live>
2022-08-01 20:33:10 +0530

    update listener types

--- eventemitter2.d.ts
+++ eventemitter2.d.ts
@@ -96,8 +96,8 @@ export interface ListenToOptions {
 }
 
 export interface GeneralEventEmitter{
-    addEventListener: (event: event, handler: ListenerFn) => void,
-    removeEventListener: (event: event, handler: ListenerFn) => void
+    addEventListener(event: event, handler: ListenerFn): this,
+    removeEventListener(event: event, handler: ListenerFn): this
 }
 
 export interface OnOptions {
